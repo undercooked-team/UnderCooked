6e0769199dd76a611daac898ec7b1dcc9cf0af7d
fridgefin <nononoscratchcat@gmail.com>
2023-01-25 11:56:40 +0000

    FoodItem, stations and Tuple class created

---
+++ ENG1/core/src/food/FoodItem.java
@@ -0,0 +1,21 @@
+package food;
+import stations.stations;
+import java.util.HashMap;
+
+public class FoodItem {
+    public enum FoodID {
+        lettuce,
+        lettuceChop,
+        tomato,
+        tomatoChop,
+        onion,
+        onionChop,
+        meat,
+        meatCook,
+        bun,
+        none
+    }
+    //public HashMap<Tuple<FoodID,stations.stationID>, FoodID> = new HashMap<Tuple<FoodID,stations.stationID>, FoodID>();
+    ///public FoodID interaction(FoodID foodID, stations.stationID stationID){};
+}
+
---
+++ ENG1/core/src/stations/stations.java
@@ -0,0 +1,10 @@
+package stations;
+
+public class stations {
+    public enum stationID{
+        fry,
+        cut,
+        counter,
+        bin
+    }
+}
