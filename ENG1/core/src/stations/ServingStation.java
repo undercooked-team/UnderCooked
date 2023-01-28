f5e6c9974a7f6c1e5e2d433310c981308f345be6
OwenTho <owen_thomas@btinternet.com>
2023-01-28 17:33:42 +0000

    Setup for ServingStation, where the player will serve a customer their order.

--- ENG1/core/src/stations/ServingStation.java
+++ ENG1/core/src/stations/ServingStation.java
@@ -3,18 +3,41 @@ package stations;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
 import cooks.Cook;
+import food.FoodStack;
 import interactions.InputKey;
 
-public class ServingStation extends CounterStation {
+public class ServingStation extends Station {
+
+    private String request;
+    //private Customer customer;
     public ServingStation(float width, float height, Body body, Rectangle rectangle) {
         super(width, height, body, rectangle);
     }
 
+    /*public boolean hasCustomer() {
+        return !(customer == null);
+    }*/
+
+    /*public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }*/
+
     @Override
     public void interact(Cook cook, InputKey.InputTypes inputType) {
-        // Only call super if the input is "USE"
+        // USE to see request, or submit request
         if (inputType == InputKey.InputTypes.USE) {
-            super.interact(cook, inputType);
+            // First make sure there is actually a request on this counter.
+            if (request != null) {
+                // If there is a request, then compare the two.
+                if (request == cook.foodStack.toString()) {
+                    // If it's correct, then the customer will take the food and leave.
+                    request = null;
+                    cook.foodStack.clearStack();
+                    //customer.leave();
+                } else {
+                    // If not, then display the customer's request.
+                }
+            }
         }
     }
 }
