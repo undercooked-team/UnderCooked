625837cda21a6c71a05ae26e615730ae41fda649
hs1955 <hs1955@york.ac.uk>
2023-01-28 13:28:07 +0000

    Here's half of the recipes. I'm still making a better way to generate them, but this is an additional feature.

--- ENG1/core/src/recipes/Recipe.java
+++ ENG1/core/src/recipes/Recipe.java
@@ -1,6 +1,9 @@
 package recipes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import food.FoodStack;
 import food.FoodItem.FoodID;
@@ -33,8 +36,68 @@ public class Recipe {
 			);
 			recipes.put("Plain Burger",
 				new String[] {
-					new FoodStack(FoodID.bun, FoodID.meatCook, FoodID.bun).toString()
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.bottomBun).toString()
+				}
+			);
+			recipes.put("Lettuce Burger",
+				new String[] {
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.lettuceChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.lettuceChop, FoodID.meatCook, FoodID.bottomBun).toString()
+				}
+			);
+			recipes.put("Onion Burger",
+				new String[] {
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.onionChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.onionChop, FoodID.meatCook, FoodID.bottomBun).toString()
+				}
+			);
+			recipes.put("Tomato Burger",
+				new String[] {
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.tomatoChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.tomatoChop, FoodID.meatCook, FoodID.bottomBun).toString()
+				}
+			);
+			recipes.put("Lettuce Onion Burger",
+				new String[] {
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.lettuceChop, FoodID.onionChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.onionChop, FoodID.lettuceChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.lettuceChop, FoodID.meatCook, FoodID.onionChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.onionChop, FoodID.meatCook, FoodID.lettuceChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.onionChop, FoodID.lettuceChop, FoodID.meatCook, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.lettuceChop, FoodID.onionChop, FoodID.meatCook, FoodID.bottomBun).toString()
+				}
+			);
+			recipes.put("Lettuce Onion Burger",
+				new String[] {
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.lettuceChop, FoodID.onionChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.meatCook, FoodID.onionChop, FoodID.lettuceChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.lettuceChop, FoodID.meatCook, FoodID.onionChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.onionChop, FoodID.meatCook, FoodID.lettuceChop, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.onionChop, FoodID.lettuceChop, FoodID.meatCook, FoodID.bottomBun).toString(),
+					new FoodStack(FoodID.topBun, FoodID.lettuceChop, FoodID.onionChop, FoodID.meatCook, FoodID.bottomBun).toString()
 				}
 			);
 	}
+
+	private <T> ArrayList<ArrayList<T>> allCombos(T... stuff) {
+		ArrayList<T> stuffList = new ArrayList<T>(Arrays.asList(stuff));
+		ArrayList<ArrayList<T>> storage = new ArrayList<ArrayList<T>>();
+		allCombosR(storage, stuffList);
+		return storage;
+	}
+	private <T> ArrayList<T> allCombosR(ArrayList<ArrayList<T>> storage, ArrayList<T> remaining) {
+		ArrayList<T> myList = new ArrayList<T>();
+		for (int i = 0; i < remaining.size()-1; i++) {
+			myList.add(remaining.get(i));
+			ArrayList<T> newRemaining = (ArrayList<T>) remaining.clone();
+			newRemaining.remove(i);
+			myList.addAll(allCombosR(storage, newRemaining));
+			storage.add(myList);
+		}
+		return myList;
+	}
+	// public static void main(String[] args) {
+	// 	ArrayList<ArrayList<FoodID>> list = allCombos(FoodID.meatCook, FoodID.lettuceChop, FoodID.onionChop);
+	// 	System.out.println(list);
+	// }
 }
