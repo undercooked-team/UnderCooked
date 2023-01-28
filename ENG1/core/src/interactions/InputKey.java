717b2e280f6442eb4ae21bc257a30368b1c3f617
OwenTho <owen_thomas@btinternet.com>
2023-01-28 01:07:25 +0000

    Added an InputKey HashMap which allows new keys to be added easier. In this case, I added 3 different forms of Interactions that the Cook can do.

---
+++ ENG1/core/src/interactions/InputKey.java
@@ -0,0 +1,27 @@
+package interactions;
+
+public class InputKey {
+
+    public enum InputTypes {
+        PUT_DOWN,
+        PICK_UP,
+        USE
+    }
+
+    private int key;
+    private InputTypes inputType;
+
+    public InputKey(InputTypes inputType, int key) {
+        this.key = key;
+        this.inputType = inputType;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public InputTypes getType() {
+        return inputType;
+    }
+
+}
