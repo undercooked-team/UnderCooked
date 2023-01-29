6cef84c3fd084f0eaefb3b8dd8d53782b25df0ae
hs1955 <hs1955@york.ac.uk>
2023-01-29 22:32:20 +0000

    BinStation done.

--- ENG1/core/src/stations/CookInteractable.java
+++ ENG1/core/src/stations/CookInteractable.java
@@ -7,7 +7,9 @@ import cooks.GameEntity;
 
 import interactions.InputKey;
 
+/** The CookInteractable Class. This is basically stations and pantries.*/
 public abstract class CookInteractable extends GameEntity {
+    /** The rectangle representing the collision of this interactable. */
     protected Rectangle interactRect;
 
     public CookInteractable(float width, float height, Body body, Rectangle rect) {
