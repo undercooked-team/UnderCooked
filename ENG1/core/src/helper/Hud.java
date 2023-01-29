8ee543fba0c3347a4903c5f0d54038fab93a8484
hs1955 <hs1955@york.ac.uk>
2023-01-29 21:15:10 +0000

    Hud.java Commented

--- ENG1/core/src/helper/Hud.java
+++ ENG1/core/src/helper/Hud.java
@@ -7,12 +7,22 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+/** The Hud Class. {@link GameHud} inherits this class. */
 public class Hud {
 
+    /** Responsible for holding the table to render. */
     protected Stage stage;
+    /** The viewport of the Hud. */
     protected Viewport viewport;
+    /** Sizes and positions children using position constraints.
+     * @see {@link Table}
+     */
     protected Table table;
 
+    /**
+     * Hud Constructor.
+     * @param batch The SpriteBatch to render on this Hud.
+     */
     public Hud(SpriteBatch batch) {
         viewport = new FitViewport(Constants.V_Width, Constants.V_Height, new OrthographicCamera());
         stage = new Stage(viewport, batch);
@@ -24,6 +34,7 @@ public class Hud {
         stage.addActor(table);
     }
 
+    /** Render this Hud.*/
     public void render() {
         stage.draw();
     }
