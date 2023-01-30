d7647b3887aff3c57fce38521135ca9b5c425fd5
OwenTho <owen_thomas@btinternet.com>
2023-01-30 01:40:24 +0000

    Parameters added to the JavaDocs in Util.

--- ENG1/core/src/helper/Util.java
+++ ENG1/core/src/helper/Util.java
@@ -4,7 +4,11 @@ package helper;
 public class Util {
     /**
      * Calculates the distance between 2 points.
-     * @return float : The distance between the 2 points.
+     * @param x1 The x coordinate of the first point
+     * @param y1 The y coordinate of the first point
+     * @param x2 The x coordinate of the second point
+     * @param y2 The y coordinate of the second point
+     * @return float: The distance between the 2 points.
      */
     public static float distancePoints(float x1, float y1, float x2, float y2) {
         return (float) Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
@@ -12,7 +16,10 @@ public class Util {
 
     /**
      * Returns a string containing the given time elapsed in the parameters.
-     * @return String : The time in hrs:mins:secs
+     * @param hours The time's in hours.
+     * @param minutes The time's minutes.
+     * @param seconds The time's seconds.
+     * @return {@link String} : The time in mins:secs if hrs == 0, or hrs:mins:secs if hrs > 0.
      */
     public static String formatTime(int hours, int minutes, int seconds) {
         String timeString = "";
