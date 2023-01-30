9cc6054448e6932da17c3652d73c6e61cbd7ab31
OwenTho <owen_thomas@btinternet.com>
2023-01-30 03:59:53 +0000

    CounterStation JavaDoc complete.

--- ENG1/core/src/stations/CounterStation.java
+++ ENG1/core/src/stations/CounterStation.java
@@ -11,14 +11,41 @@ import food.FoodStack;
 import game.GameSprites;
 import interactions.InputKey;
 
+/**
+ * The {@link CounterStation} class, where the {@link cooks.Cook}
+ * can place down the {@link FoodItem}s that they are holding.
+ */
 public class CounterStation extends Station {
 
     FoodStack foodStack;
+
+    /**
+     * The constructor for the {@link CounterStation}. It sets up the
+     * {@link FoodStack} for the {@link CounterStation}.
+     * @param rectangle The collision and interaction area of the {@link PreparationStation}.
+     */
     public CounterStation(Rectangle rectangle) {
         super(rectangle);
         foodStack = new FoodStack();
     }
 
+    /**
+     * The function that allows a {@link Cook} to interact with the {@link CounterStation}.
+     *
+     * <br><br>If the {@link Cook} interacts using {@link InputKey.InputTypes#USE},
+     * they will swap stacks with the {@link CounterStation}.
+     *
+     * <br><br>If the {@link Cook} interacts using {@link InputKey.InputTypes#PICK_UP},
+     * they will take the top {@link FoodItem} of the {@link FoodStack}
+     * from {@link CounterStation} and put it on their own {@link FoodStack}.
+     *
+     * <br><br>If the {@link Cook} interacts using {@link InputKey.InputTypes#PUT_DOWN},
+     * they will take the top {@link FoodItem} of their {@link FoodStack}
+     * put it on the {@link CounterStation}'s {@link FoodStack}.
+     * @param cook The cook that interacted with the {@link CookInteractable}.
+     * @param inputType The type of {@link InputKey.InputTypes} the player made with
+     *                  the {@link CookInteractable}.
+     */
     @Override
     public void interact(Cook cook, InputKey.InputTypes inputType) {
         // If Cook is using the put down input, put down the item in the top of their stack
@@ -42,6 +69,13 @@ public class CounterStation extends Station {
         }
     }
 
+    /**
+     * The function used to render the {@link PreparationStation}.
+     *
+     * <br>This function renders the {@link FoodStack} in a similar way
+     * as the {@link Cook} renders their own.
+     * @param batch The {@link SpriteBatch} used to render.
+     */
     @Override
     public void render(SpriteBatch batch) {
         // Render using the same method as the Cook does.
