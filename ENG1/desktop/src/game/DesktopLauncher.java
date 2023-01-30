ad4065457eb515fe1e79312ad1914efa02dcaae6
OwenTho <owen_thomas@btinternet.com>
2023-01-30 00:59:38 +0000

    DesktopLauncher JavaDoc.

--- ENG1/desktop/src/game/DesktopLauncher.java
+++ ENG1/desktop/src/game/DesktopLauncher.java
@@ -5,7 +5,13 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import helper.Constants;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/** The launcher for the desktop version of the game. */
 public class DesktopLauncher {
+    /**
+     * The function that is called when the executable jar file
+     * is opened that starts running the game using {@link Boot}.
+     * @param arg Arguments for running the game's jar file.
+     */
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
