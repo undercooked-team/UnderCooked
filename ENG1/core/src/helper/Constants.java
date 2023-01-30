735190b2f49a10caa2b47803b31d4f359832fcae
OwenTho <owen_thomas@btinternet.com>
2023-01-30 03:33:11 +0000

    Constants JavaDoc completed.

--- ENG1/core/src/helper/Constants.java
+++ ENG1/core/src/helper/Constants.java
@@ -2,15 +2,21 @@ package helper;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * A class for variables that remain Constant
+ * that are needed all across the code.
+ */
 public class Constants {
 
-    /** Pixels Per Move??? */
+    /** Pixels Per Metre. */
     public static final float PPM = 32.0f;
-    /** The ViewPort / Window Width */
+    /** The ViewPort / Window Width. */
     public static final int V_Width = 960;
-    /** The ViewPort / Window Height */
+    /** The ViewPort / Window Height. */
     public static final int V_Height = 640;
 
+    /** The scale of the {@link customers.Customer}s.*/
     public static  final float customerScale = 0.2f;
+    /** The {@link customers.Customer} default spawn position */
     public static final Vector2 customerSpawn= new Vector2(425,470);
 }
