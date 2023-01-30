cd7ea72c4d554d8eae20e6dfc3a37308758e74c9
OwenTho <owen_thomas@btinternet.com>
2023-01-30 23:51:42 +0000

    Customer JavaDoc completed.

--- ENG1/core/src/customers/Customer.java
+++ ENG1/core/src/customers/Customer.java
@@ -10,14 +10,25 @@ import food.Recipe;
 import game.GameScreen;
 import helper.Constants;
 
+/**
+ * A {@link Customer} has a request that they want
+ * to be served by the player.
+ */
 public class Customer {
 
+    /** The position of the Customer. */
     public Vector2 position;
+    /** The {@link Sprite} of the {@link Customer}. */
     public Sprite sprite;
+    /** The name of the {@link Recipe} that the {@link Customer}
+     * is requesting. */
     private String request;
 
-    private boolean canDraw = false;
-
+    /**
+     * The constructor for the {@link Customer}.
+     * <br>Randomly picks out a {@link Recipe} as a request.
+     * @param sprite The {@link Sprite} of the {@link Customer}.
+     */
     public Customer(Sprite sprite)
     {
         this.sprite = sprite;
@@ -25,30 +36,38 @@ public class Customer {
         this.request = Recipe.randomRecipe();
     }
 
+    /**
+     * Another constructor for the {@link Customer}, with a specified position.
+     * <br>Randomly picks out a {@link Recipe} as a request.
+     * @param sprite The {@link Sprite} of the {@link Customer}.
+     * @param position A {@link Vector2} position of the {@link Customer}.
+     */
     public Customer(Sprite sprite, Vector2 position) {
         this(sprite);
         this.position = position;
     }
 
-    public void setCanDraw(boolean value)
-    {
-        this.canDraw = value;
-
-    }
-
-    public void randomRecipe() {
-
-    }
-
+    /**
+     * Renders the {@link Customer} at their {@link #position}.
+     * @param batch The {@link SpriteBatch} of the game.
+     */
     public void render(SpriteBatch batch) {
         sprite.setPosition(position.x-sprite.getWidth()/2, position.y-sprite.getHeight()/2);
         sprite.draw(batch);
     }
 
+    /**
+     * Getter for the {@code x} position of the {@link Customer}.
+     * @return {@code float} : The {@code x} of the {@link Customer}.
+     */
     public float getX() {
         return position.x;
     }
 
+    /**
+     * Getter for the {@code y} position of the {@link Customer}.
+     * @return {@code float} : The {@code y} of the {@link Customer}.
+     */
     public float getY() {
         return position.y;
     }
