1965b998196e56fc5a41be28ff3ccbfe1fb186d6
OwenTho <owen_thomas@btinternet.com>
2023-01-30 04:02:10 +0000

    BinStation JavaDoc complete.

--- ENG1/core/src/stations/BinStation.java
+++ ENG1/core/src/stations/BinStation.java
@@ -10,10 +10,24 @@ import interactions.InputKey;
  */
 public class BinStation extends Station {
 
+    /**
+     * The constructor for the {@link BinStation}.
+     * @param rectangle The collision and interaction area of the {@link Station}.
+     */
     public BinStation(Rectangle rectangle) {
         super(rectangle);
     }
 
+    /**
+     * The interact function for the {@link BinStation}.
+     *
+     * This takes the top item from the {@link Cook}'s {@link food.FoodStack}
+     * if they use either the {@link InputKey.InputTypes#USE} or
+     * {@link InputKey.InputTypes#PUT_DOWN} keys.
+     * @param cook The cook that interacted with the {@link CookInteractable}.
+     * @param inputType The type of {@link InputKey.InputTypes} the player made with
+     *                  the {@link CookInteractable}.
+     */
     @Override
     public void interact(Cook cook, InputKey.InputTypes inputType) {
         // Only bin if user inputs USE or PUT_DOWN
